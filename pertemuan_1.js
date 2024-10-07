0a5442c7fe2906e3d49c432b252ded02726690b7
Muhammad Reksa Ariansyah <reksa.ariansyah@biofarma.co.id>
2024-10-07 19:59:45 +0700

    first commit

---
+++ pertemuan_1.js
@@ -0,0 +1,34 @@
+let myName = "M Reksa Ariansyah";
+
+myName = "Jude Bellingham";
+
+const namaSaya = "Muhammad Reksa Ariansyah";
+
+function testingSum(param1, param2) {
+  console.log(param1 + param2);
+}
+
+function hitungBMI(beratBadan, tinggiBadan) {
+  const tinggiValue = tinggiBadan / 100;
+  const hasil = beratBadan / (tinggiValue * tinggiValue);
+
+  console.log(hasil);
+
+  if (hasil < 18.5) {
+    console.log("Berat Badan Anda Kurang");
+  } else if (hasil >= 18.5 && hasil <= 24.9) {
+    console.log("Normal");
+  } else if (hasil >= 25 && hasil <= 29.9) {
+    console.log("Berat Badan anda berlebih");
+  } else {
+    console.log("Anda Obesitas");
+  }
+}
+
+function loopingAngka(data) {
+  for (let i = 1; i <= data; i++) {
+    console.log("ini adalah angka ke" + i);
+  }
+}
+
+loopingAngka(10);
